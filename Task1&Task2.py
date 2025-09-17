c0ea76dbd918ee629e84b703f24be4c34609d3e1
Nikhil Kirar <nikhilkirar6776@gmail.com>
2025-09-17 05:41:01 +0530

    Add files via upload

---
+++ Task1&Task2.py
@@ -0,0 +1,48 @@
+#1.  Takes two numbers as input from the user.
+import re
+
+# a = int(input("enter number: "))
+# b = int(input("enter another number: "))
+#
+# print(a , b)
+
+
+#Performs the basic mathematical operations on these two numbers:
+# o	Addition
+# o	Subtraction
+# o	Multiplication
+# o	Division
+
+# ans
+# a = int(input("enter number: "))
+# b = int(input("enter another number: "))
+#
+# print(a + b)
+# print(a - b)
+# print(a * b)
+# print(a / b)
+#
+# print(type(a))
+# print(type(b))
+
+#@2 - Chapter 2
+
+#Takes a user's first name and last name as input.
+
+# a = input("first Name: ")
+# b= input("last name: ")
+#
+# print(a,b)
+#
+# # 2.  Concatenates the first name and last name into a full name.
+# a = input("first Name: ")
+# b= input("last name: ")
+#
+# print(a +" "+ b)
+
+#3.  Prints a personalized greeting message using the full name.
+
+# a = input("enter first name:")
+# b = input("enter last name:")
+#
+# print("hello,",a,b,"!Welcome to python programming" )
